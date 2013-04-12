b056f38ea65f7add6e990422091003dec6be07c0
Sumin Byeon <suminb@gmail.com>
2013-04-12 00:29:47 -0700

    Getting started.

--- dummy-login-ios/ViewController.m
+++ dummy-login-ios/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize data;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +26,43 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+//
+// Copied from http://stackoverflow.com/questions/5537297/ios-how-to-perform-a-http-post-request
+//
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+//    [self.data setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)d {
+//    [self.data appendData:d];
+
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"")
+                                 message:[error localizedDescription]
+                                delegate:nil
+                       cancelButtonTitle:NSLocalizedString(@"OK", @"")
+                       otherButtonTitles:nil] show];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSString *responseText = [[NSString alloc] initWithData:self.data encoding:NSUTF8StringEncoding];
+    
+}
+
+// Handle basic authentication challenge if needed
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+    NSString *username = @"username";
+    NSString *password = @"password";
+    
+    NSURLCredential *credential = [NSURLCredential credentialWithUser:username
+                                                             password:password
+                                                          persistence:NSURLCredentialPersistenceForSession];
+    [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
 }
 
 @end
